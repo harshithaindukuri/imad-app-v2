9aa15b8afdd37a6abfbe1164a658a879f6047a58
harshithaindukuri <harshithaindukuri@gmail.com>
2017-03-05 07:47:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('article one ' ,function(req,res)
 });
 app.get('article two ' ,function(req,res)
 {
-    res.send("artice two has been called it will be served here");
+    res.send('artice two has been called it will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
