a22aecdaa6a9617469945eac3e191a95a9ee6cee
harshithaindukuri <harshithaindukuri@gmail.com>
2017-03-23 17:05:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/article2', function (req, res) {
+app.get('/article2', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 
