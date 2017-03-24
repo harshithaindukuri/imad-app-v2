22a56a6069f563572a6976a5dcaf0ad8dca9e5c6
harshithaindukuri <harshithaindukuri@gmail.com>
2017-03-24 05:46:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
 console.log('Loaded!');
 var v = document.getElementById('image');
 var move=0;
-function mr(){
- move=move+10;    
- v.style.marginLeft=move+ 'px';   
-}
+var moveRight = function(){
+    move=move+1;
+    v.style.marginLeft=move;
+};
 v.onclick = function(){
   
-  var interval = setInterval(mr,100);
+  var interval = setInterval(moveRight,100);
   
 };
