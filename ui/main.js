84fce1e644b88d7636973b3f22d0d992f56e6d3a
harshithaindukuri <harshithaindukuri@gmail.com>
2017-03-24 11:48:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 var button = document.getElementById("c");
-var c=0;
+var b=0;
 button.onclick=function(){
-c=c+1;
+b=b+1;
 var span = document.getElementById("count");
-span.innerHTML=c.toString();
+span.innerHTML=b.toString();
 };
 
