0818df5ee4d34c1b589bee7e1812051f2aad94d0
harshithaindukuri <harshithaindukuri@gmail.com>
2017-03-24 12:14:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,21 @@
 var button = document.getElementById("c");
 var b=0;
 button.onclick=function(){
-b=b+1;
-var span = document.getElementById("count");
-span.innerHTML=b.toString();
+var request = new XMLHttpRequest(); 
+request.onreaddystatechange=function(){
+ if(request.readystate === XMLHttpRequest.DONE)
+    {
+       if(request.status === 200)
+       {
+           b=b+1;
+           var span = document.getElementById("count");
+           span.innerHTML=b.toString();
+       }
+    }
+    
 };
+request.open('get','http://harshithaindukuri.imad.hasura-app.io/counter',true);
+request.send(null);
+};
+
 
