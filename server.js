7c3df5c30cddfb7ed748ea50d6a6713f2e55d161
harshithaindukuri <harshithaindukuri@gmail.com>
2017-03-24 10:10:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,9 +37,10 @@ var htmltemp=`<!Doctype html>
 `;
 return htmltemp;
 }
+var counter =0;
 app.get('/counter',function(req,res){
-   count++;
-   res.send(count.toString());
+  counter=counter+1;
+  res.send(counter.toString());
 });
 
 app.get('/', function (req, res) {
